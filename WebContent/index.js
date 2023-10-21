9b9239291a3ba55dbf88c5e85a114fc72c79c15f
Henry Reyes <reyesh2@uci.edu>
2023-10-21 13:42:29 -0700

    search bar protoype, login button

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,11 +1,11 @@
 
 
-    let movieElement = jQuery("#movie-link");
+let movieElement = jQuery("#movie-link");
 
-    movieElement.append('<a href=' + '"movie-list.html">'
+movieElement.append('<a href=' + '"movie-list.html">'
         + "Click for top 20 movies"+     // display star_name for the link text
         '</a>');
 
-    let moviePage = jQuery(".home");
+let moviePage = jQuery(".home");
 
 
--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class LoginServlet {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/json");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter out = response.getWriter();
+
+        try (Connection conn = dataSource.getConnection()) {
+
+
+            String query = "SELECT email, password FROM customers WHERE email = ? AND password = ?";
+
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            ResultSet rs = statement.executeQuery();
+            System.out.print(rs.toString());
+
+
+            JsonObject jsonObject = new JsonObject();
+
+            if (rs.next()) {
+                jsonObject.addProperty("status", "success");
+                jsonObject.addProperty("message", "Logged in successfully!");
+                response.sendRedirect("http://localhost:8080/fabflix_war/index.html");
+                response.setStatus(200);
+            } else {
+                // User not found
+                jsonObject.addProperty("status", "fail");
+                jsonObject.addProperty("message", "Invalid email or password!");
+                jsonObject.addProperty("input", username + " " + password);
+                response.setStatus(401);  // Unauthorized
+            }
+
+            rs.close();
+            statement.close();
+
+            out.write(jsonObject.toString());
+
+        } catch (Exception e) {
+
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            request.getServletContext().log("Error:", e);
+            response.setStatus(500);
+
+        } finally {
+            out.close();
+        }
+    }
 }
