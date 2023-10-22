e11b3b2c4b6eeac24a0ff970708b89fea446addc
Henry Reyes <reyesh2@uci.edu>
2023-10-22 08:56:15 -0700

    update nav bar

--- WebContent/stars/stars.js
+++ WebContent/stars/stars.js
@@ -61,6 +61,6 @@ function handleStarResult(resultData) {
 jQuery.ajax({
     dataType: "json", // Setting return data type
     method: "GET", // Setting request method
-    url: "api/stars", // Setting request url, which is mapped by StarsServlet in Stars.java
+    url: "../api/stars", // Setting request url, which is mapped by StarsServlet in Stars.java
     success: (resultData) => handleStarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
 });
